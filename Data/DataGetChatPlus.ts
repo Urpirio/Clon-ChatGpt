f7a7ce547b8f01da26bae40f4ccdae70739f0dd9
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-07-22 21:45:14 -0400

    Se agrego el array de datos de la pantalla de GptPlus

---
+++ Data/DataGetChatPlus.ts
@@ -0,0 +1,18 @@
+
+export const DataGetChatPlus = [
+    {
+        Title:'Todo lo que se incluye en el plan gratuito'
+    },
+    {
+        Title: 'Límites extendidos en mensajería, cargas de archivos, análisis de datos avanzado y generación de imágenes'
+    },
+    {
+        Title: 'Modo de voz estándar y avanzado con video y uso compartido de pantalla'
+    },
+    {
+        Title: ' Acceso a varios modelos de razonamiento (como la serie o3 de OpenAI, o4-mini y una vista previa de investigación de GPT-4.5)'
+    },
+    {
+        Title: ' Acceso a varios modelos de razonamiento (como la serie o3 de OpenAI, o4-mini y una vista previa de investigación de GPT-4.5)'
+    }
+]
